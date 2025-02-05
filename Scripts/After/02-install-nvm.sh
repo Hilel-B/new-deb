8cefe37c2f126bc23b9b21e5ddd1583788c0b593
Hilel-B <94998820+Hilel-B@users.noreply.github.com>
2025-02-05 01:50:11 +0100

    Update 02-install-nvm.sh

--- Scripts/After/02-install-nvm.sh
+++ Scripts/After/02-install-nvm.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh
-bash && source ~/.bashrc
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+source ~/.bashrc
 nvm install --lts
 node -v && npm -v
